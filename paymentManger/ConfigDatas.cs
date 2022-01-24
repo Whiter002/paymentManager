80e1ee748dcb823042fc66bec6a42e1d57f44d88
Whiter002 <92290346+Whiter002@users.noreply.github.com>
2022-01-24 10:24:30 +0900

    クラス作成

---
+++ paymentManger/ConfigDatas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paymentManger
+{
+    static internal class ConfigDatas
+    {
+        //分類情報などのコンフィグデータをここから呼び出すために作られたクラスです。
+    }
+}
